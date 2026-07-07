98d66b0bbd1a8b1482d4a978af4221e0b0e875ed
JosueOrtiz1223 <Josue.ortiz@ucr.ac.cr>
2026-07-06 20:02:46 -0600

    Se crea un pequeño primer cambio donde nos preparamos para separar el archivo en 3 partes. Por ahora no es funcional

--- examen2-2026-main/ExamTwo/ExamTwo/Controllers/CoffeeMachineController.cs
+++ examen2-2026-main/ExamTwo/ExamTwo/Controllers/CoffeeMachineController.cs
@@ -1,107 +1,51 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+// using ExamTwo.Repositories;
+// using ExamTwo.Services;
+//using ExamTwo.Models;
 
 namespace ExamTwo.Controllers
 {
-    public class CoffeeMachineController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class CoffeeMachineController : ControllerBase
     {
+        private readonly ICoffeeMachineRepository _repo;
+        private readonly ICoffeeMachineService _service;
 
-        private readonly Database _db;
-
-        public CoffeeMachineController(Database db)
+        public CoffeeMachineController(ICoffeeMachineRepository repo, ICoffeeMachineService service)
         {
-            _db = db;
+            _repo = repo;
+            _service = service;
         }
 
-        [HttpGet("getCoffees")]
+        [HttpGet("Coffees")]
+        public ActionResult<Dictionary<string, int>> GetCoffee()
+        {
+            return Ok(_repo.GetCoffeeTypes());
+        }
+
+        [HttpGet("CoffeePrices")]
         public ActionResult<Dictionary<string, int>> GetCoffeePrices()
         {
-            return Ok(_db.keyValues);
+            return Ok(_repo.GetCoffeePrices());
         }
 
-        [HttpGet("getCoffeePricesInCents")]
-        public ActionResult<Dictionary<string, int>> GetCoffeePricesInCents()
+        [HttpGet("Quantity")]
+        public ActionResult<Dictionary<int, int>> GetQuantity()
         {
-            return Ok(_db.keyValues2);
-        }
-
-        [HttpGet("getQuantity")]
-        public ActionResult<Dictionary<string, int>> GetQuantity()
-        {
-            return Ok(_db.keyValues3);
+            return Ok(_repo.GetCoinInventory());
         }
 
         [HttpPost("buyCoffee")]
         public ActionResult<string> BuyCoffee([FromBody] OrderRequest request)
         {
-            if (request.Order == null || request.Order.Count == 0)
-                return BadRequest("Ordem vacia.");
+            var result = _service.BuyCoffee(request);
+            if (result.Success)
+                return Ok(result.Message);
 
-            if (request.Payment.TotalAmount <= 0)
-                return BadRequest("Dinero insuficiente ");
-
-            try
-            {
-                var costoTotal = request.Order.Sum(o => _db.keyValues2.First(c => c.Key == o.Key).Value * o.Value);
-
-                if (request.Payment.TotalAmount < costoTotal)
-                { 
-                    return BadRequest("Dinero insuficiente ");
-                }
-
-
-                foreach (var cafe in request.Order)
-                {
-                    var selected = _db.keyValues.First(c => c.Key == cafe.Key).Key;
-                    if (cafe.Value > _db.keyValues[selected])
-                    {
-                        return $"No hay suficientes {selected} en la máquina.";
-                    }
-                    _db.keyValues[selected] -= cafe.Value;
-                }
-
-                var change = request.Payment.TotalAmount - costoTotal;
-                String result = $"Su vuelto es de: {change} colones. Desglose:";
-
-                foreach (var coin in _db.keyValues3.Keys.OrderByDescending(c => c))
-                {
-                    var count = Math.Min(change / coin, _db.keyValues3[coin]);
-                    if (count > 0)
-                    {
-                        result +=  $" {count} moneda de {coin},  ";              
-                        change -= coin * count;
-                    }
-                }
-
-
-                if (change > 0)
-                {
-                    return StatusCode(500, "No hay suficiente cambio en la máquina.");
-                }
-
-                return Ok(result);
-            }
-            catch (ArgumentException ex)
-            {
-                return BadRequest(ex.Message);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, ex.Message);
-            }
+            return StatusCode(result.StatusCode, result.Message);
         }
-    }
-
-    public class OrderRequest
-    {
-        public Dictionary<string, int> Order { get; set; }
-        public Payment Payment { get; set; }
-    }
-
-    public class Payment
-    {
-        public int TotalAmount { get; set; }
-        public List<int> Coins { get; set; }
-        public List<int> Bills { get; set; }
     }
 }
---
+++ examen2-2026-main/ExamTwo/ExamTwo/Models/OrderRequest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace ExamTwo.Models;
+    
+    public class OrderRequest
+    {
+        public Dictionary<string, int> Order { get; set; }
+        public Payment Payment { get; set; }
+    }
+
+    public class Payment
+    {
+        public int TotalAmount { get; set; }
+        public List<int> Coins { get; set; }
+        public List<int> Bills { get; set; }
+    }
